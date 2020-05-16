3cf113a729239dfc09c2b1f8ae94547b66ee0e02
khanhdoth <khanh.doth@gmail.com>
2020-05-16 13:30:37 +0000

    u

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :el_kube, ElKube.Repo,
   username: "postgres",
   password: "postgres",
   database: "el_kube_dev",
-  hostname: "localhost",
+  hostname: "172.17.0.1",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
